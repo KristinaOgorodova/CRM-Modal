aeffc79710cc91b3e5bc23dd22b4d3ef44c426ad
Kristina Ogorodova <skrok@inbox.ru>
2022-11-02 21:08:57 +0300

    homework finish

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 'use strict'
+
+const header = document.querySelector('.add-product');
+const discountCheckbox = document.querySelector('#discount');
+const discountQty = document.querySelector('#number');
+const totalSum = document.querySelector('.add-form__amount');
+const form = document.querySelector('.add-form');
