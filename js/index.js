4ef4f98cfaf056b89f09d22510bf8e76f391cdac
Kristina Ogorodova <skrok@inbox.ru>
2022-11-08 21:33:41 +0300

    homework is done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,34 @@
-'use strict'
+'use strict';
 
 const header = document.querySelector('.add-product');
 const discountCheckbox = document.querySelector('#discount');
 const discountQty = document.querySelector('#number');
 const totalSum = document.querySelector('.add-form__amount');
-const form = document.querySelector('.add-form');
+const form = document.querySelector('.add');
+
+const addCardBtn = document.querySelector('.table-select__add-btn');
+const modal = document.querySelector('.overlay');
+const modalCloseBtn = document.querySelector('.add-product__close-btn')
+
+addCardBtn.addEventListener('click', () => {
+  modal.classList.add('overlay_open');
+});
+
+modalCloseBtn.addEventListener('click', () => {
+  modal.classList.remove('overlay_open');
+});
+
+form.addEventListener('click', event => {
+  event.stopPropagation();
+})
+
+modal.addEventListener('click', ()=> {
+  modal.classList.remove('overlay_open');
+});
+
+
+
+
+
+
+
