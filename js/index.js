5109c127dc9939f9820147aaaaa71dcfb79411ad
Kristina Ogorodova <skrok@inbox.ru>
2022-11-11 11:35:21 +0300

    делегирование

--- js/index.js
+++ js/index.js
@@ -14,18 +14,35 @@ addCardBtn.addEventListener('click', () => {
   modal.classList.add('overlay_open');
 });
 
-modalCloseBtn.addEventListener('click', () => {
-  modal.classList.remove('overlay_open');
+// modalCloseBtn.addEventListener('click', () => {
+//   modal.classList.remove('overlay_open');
+// });
+// form.addEventListener('click', event => {
+//   event.stopPropagation();
+// })
+// modal.addEventListener('click', ()=> {
+//   modal.classList.remove('overlay_open');
+// });
+
+modal.addEventListener('click', e => {
+  const target = e.target;
+  if (target === modal || target.classList.contains('add-product__close-btn')) {
+    modal.classList.remove('overlay_open');
+  }
 });
 
-form.addEventListener('click', event => {
-  event.stopPropagation();
-})
-
-modal.addEventListener('click', ()=> {
-  modal.classList.remove('overlay_open');
+const deleteItem = document.querySelectorAll('.delete');
+const row = document.querySelectorAll('tr');
+row.forEach(tr => {
+  tr.classList.add('table-row');
 });
 
+
+tableBody.addEventListener('click', e => {
+  if (e.target.closest('.delete')) {
+    e.target.closest('.table-row').remove();
+  }
+});
 
 
 
