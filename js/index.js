54f9ac2f23177d73e7249fc1d35db203c4421816
Kristina Ogorodova <skrok@inbox.ru>
2022-11-11 19:15:31 +0300

    error

--- js/index.js
+++ js/index.js
@@ -1,5 +1,93 @@
 'use strict';
 
+const products = [
+  {
+    'id': 253842678,
+    'title': 'Смартфон Xiaomi 11T 8/128GB',
+    'price': 27000,
+    'description': 'Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.',
+    'category': 'mobile-phone',
+    'discont': false,
+    'count': 3,
+    'units': 'шт',
+    'sum': 2000,
+    'images': {
+      'small': 'img/smrtxiaomi11t-m.jpg',
+      'big': 'img/smrtxiaomi11t-b.jpg',
+    },
+  },
+  {
+    'id': 296378448,
+    'title': 'Радиоуправляемый автомобиль Cheetan',
+    'price': 4000,
+    'description': 'Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет',
+    'category': 'toys',
+    'discont': 5,
+    'count': 1,
+    'units': 'шт',
+    'sum': 2000,
+    'images': {
+      'small': 'img/cheetancar-m.jpg',
+      'big': 'img/cheetancar-b.jpg',
+    },
+  },
+  {
+    'id': 215796548,
+    'title': 'ТВ приставка MECOOL KI',
+    'price': 12400,
+    'description': 'Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D',
+    'category': 'tv-box',
+    'discont': 15,
+    'count': 4,
+    'units': 'шт',
+    'sum': 2000,
+    'images': {
+      'small': 'img/tvboxmecool-m.jpg',
+      'big': 'img/tvboxmecool-b.jpg',
+    },
+  },
+  {
+    'id': 246258248,
+    'title': 'Витая пара PROConnect 01-0043-3-25',
+    'price': 22,
+    'description': 'Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.',
+    'category': 'cables',
+    'discont': false,
+    'count': 420,
+    'units': 'v',
+    'sum': 2000,
+    'images': {
+      'small': 'img/lan_proconnect43-3-25.jpg',
+      'big': 'img/lan_proconnect43-3-25-b.jpg',
+    },
+  },
+];
+
+const tableBody = document.querySelector('tbody');
+
+const createRow = (obj) => {
+  const tr = document.createElement('tr');
+
+  tr.classList.add('td');
+
+  tr.insertAdjacentHTML('beforeend',
+      `<td class="product-id">${obj.id}</td>
+    <td>${obj.title}</td>
+    <td>${obj.category}</td>
+    <td class="qty">${obj.units}</td>
+    <td class="amount">${obj.count}</td>
+    <td>${obj.price}</td>
+    <td>${obj.sum}</td>    
+    <td><button class="picture-btn"><img src="./image/carbon_no-image.svg" alt="нет картинки"></button></td>
+    <td><button class="picture-btn"><img src="./image/edit.svg" alt="редактировать" ></button></td>
+    <td><button class="picture-btn"><img src="./image/cart.svg" alt="корзина" ></button></td>`);
+  tableBody.append(tr);
+};
+
+const renderGoods = (array) => array.map(createRow);
+
+renderGoods(products);
+
 const header = document.querySelector('.add-product');
 const discountCheckbox = document.querySelector('#discount');
 const discountQty = document.querySelector('#number');
@@ -8,7 +96,6 @@ const form = document.querySelector('.add');
 
 const addCardBtn = document.querySelector('.table-select__add-btn');
 const modal = document.querySelector('.overlay');
-const modalCloseBtn = document.querySelector('.add-product__close-btn')
 
 addCardBtn.addEventListener('click', () => {
   modal.classList.add('overlay_open');
@@ -31,21 +118,21 @@ modal.addEventListener('click', e => {
   }
 });
 
-const deleteItem = document.querySelectorAll('.delete');
 const row = document.querySelectorAll('tr');
 row.forEach(tr => {
   tr.classList.add('table-row');
 });
 
+tableBody.addEventListener('click', ({target}) => {
+  if (target.closest('.delete')) {
+    const currentRow = target.closest('.table-row');
+    const productId = +currentRow.querySelector('.product-id').textContent;
+    const currentRowIndex = products.findIndex((product) => product.id === productId);
 
-tableBody.addEventListener('click', e => {
-  if (e.target.closest('.delete')) {
-    e.target.closest('.table-row').remove();
+    products.splice(currentRowIndex, 1);
+
+    currentRow.remove();
   }
 });
-
-
-
-
 
 
